743c4e7958d9bf56611a04d960d53af2a90d101a
Brijesh <brijeshpandey18@gmail.com>
2020-11-07 18:08:38 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
     if(req.url==="/welcome"){
-        res.writeHead(200).write("Welcome to Dominos!");
-        res.end();
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        //res.writeHead(200).write();
+        res.end("Welcome to Dominos!");
     }
     else if(req.url==="/contact"){
         const obj={
             phone: '18602100000',
             email: 'guestcaredominos@jublfood.com'
         };
-        res.writeHead(200).write(JSON.stringify(obj));
-        res.end();
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        //res.writeHead(200).write(JSON.stringify(obj));
+        res.end(JSON.stringify(obj));
     }
     else{
         res.writeHead(404).end("page not found");
