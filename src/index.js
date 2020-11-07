842776e5e86d262c545ebaa281b8bd519ff1c5c4
Brijesh <brijeshpandey18@gmail.com>
2020-11-07 18:00:43 +0530

    trial

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-const contacts = [
-    {
-        phone: "18602100000",
-        email: "guestcaredominos@jublfood.com"
-    }
-];
-
 
 function handleServer(req, res) {
-    if (req.url === "/welcome") {
-        res.status(200).send("Welcome to Dominos!");
-        return;
-    } else if (req.url === "/contact") {
-        res.status(200).send(contacts);
-        return;
-    } else {
-        res.status(400);
+    if(req.url==="/welcome"){
+        res.writeHead(200).write("Welcome to Dominos!");
+        res.end();
+    }
+    else if(req.url==="/contact"){
+        const obj={
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        };
+        res.writeHead(200).write(JSON.stringify(obj));
+        res.end();
+    }
+    else{
+        res.writeHead(404).end("page not found");
     }
 }
-httpServer.listen(8081);
+
+httpServer.listen(8081,()=>{console.log("Listning")});
+
 module.exports = httpServer;
